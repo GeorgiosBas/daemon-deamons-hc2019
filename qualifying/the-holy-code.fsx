822cc806a394366635d7ef0a71b00ef114cf46a5
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-02-28 21:32:55 +0200

    Add initial shuffler code.

--- qualifying/the-holy-code.fsx
+++ qualifying/the-holy-code.fsx
@@ -1,6 +1,23 @@
-open System.IO
+#r "../packages/FsRandom/lib/net45/FsRandom.dll"
+
+open FsRandom
 open System
 open System.Diagnostics
+open System.IO
+open System.Security.Cryptography
+
+let rand =
+    let csprng = RandomNumberGenerator.Create()
+    let fRNG () =
+        let arr = Array.zeroCreate 8
+        csprng.GetBytes arr
+        let n = BitConverter.ToUInt64(arr, 0)
+        n, ()
+    createState fRNG ()
+
+let shuffle arr =
+    let fShuffle = Array.shuffleInPlace arr
+    Random.get fShuffle rand
 
 [<Struct>]
 type Orientation = H | V
@@ -15,27 +32,31 @@ type Image = {
 
 let getO x = x.O
 
-let rec pairs x = seq {
-    match x with
-    | x1 :: x2 :: xs ->
-        yield [x1; x2]
-        yield! pairs xs
-    | _ -> ()
+let rec pairs (x: _ []) = seq {
+    let mutable i = 0
+    while i < x.Length - 1 do
+        yield [x.[i]; x.[i + 1]]
+        i <- i + 2
 }
 
-type SlideShow = Image list list
+type SlideShow = Image list []
 
-let solveIt (data: string list) () =
+let solveIt (data: string []) () =
     let parseLine idx l =
         let O(o) :: _ :: tags = List.ofArray l
         {Id = uint32 idx; O = o; Tags = set tags}
-    let data = data |> List.mapi (fun idx x -> x.Split(' ') |> parseLine idx)
+    let data = data |> Seq.mapi (fun idx x -> x.Split(' ') |> parseLine idx) |> Array.ofSeq
 
     let slideShows: SlideShow =
-        let h, v = List.partition (getO >> ((=) H)) data
-        let h = h |> List.map List.singleton
-        let v = v |> pairs |> List.ofSeq
-        h @ v
+        let h, v = Array.partition (getO >> ((=) H)) data
+        let h = h |> Array.map List.singleton
+        shuffle v
+        let v = v |> pairs |> Array.ofSeq
+
+        let theHolyResult = Array.append h v
+        shuffle theHolyResult
+
+        theHolyResult
 
     slideShows
     |> Seq.map (Seq.map (fun {Id = idx} -> string idx) >> String.concat " ") |> Seq.append [string slideShows.Length]
@@ -54,8 +75,8 @@ Environment.CurrentDirectory
 |> Directory.EnumerateFiles
 |> Seq.filter (Path.GetExtension >> ((=) ".txt"))
 |> Seq.map (Path.GetFileName)
-|> Seq.iter (fun x -> 
-    let data = File.ReadAllLines x |> List.ofArray |> List.tail
+|> Seq.iter (fun x ->
+    let data = File.ReadLines x |> Seq.tail |> Array.ofSeq
     let outputFileName = Path.ChangeExtension(x, ".out")
     let theHolySolution = solveIt data |> timeIt x
     File.WriteAllLines(outputFileName, theHolySolution))
