85160e4ba5720e5b5d4231a907a230f273947f08
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-02-28 20:00:29 +0200

    Add primitive domain types.

--- qualifying/the-holy-code.fsx
+++ qualifying/the-holy-code.fsx
@@ -1,14 +1,26 @@
 open System.IO
 open System
 
+[<Struct>]
+type Orientation = H | V
+
+type Image = {
+    O: Orientation
+    Tags: string Set
+}
+
+type SlideShow = Image * Image option
+
 let solveIt data = raise <| NotImplementedException()
+
+// MAGIC ENDS HERE
 
 Environment.CurrentDirectory
 |> Directory.EnumerateFiles
-|> Seq.filter (Path.GetExtension >> ((=) ".in"))
+|> Seq.filter (Path.GetExtension >> ((=) ".txt"))
 |> Seq.map (Path.GetFileName)
 |> Seq.iter (fun x -> 
-    let data = File.ReadAllLines x
-    let outputFileName = Path.ChangeExtension(x, ".out")
+    let data = File.ReadAllLines x |> List.ofArray |> List.tail
+    let outputFileName = Path.ChangeExtension(x, ".out.txt")
     let theHolySolution = solveIt data
     File.WriteAllLines(outputFileName, theHolySolution))
