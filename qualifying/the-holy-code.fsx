1abef4c05d909369c2bee646d39982bcc1070e87
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2019-02-28 20:30:41 +0200

    Add timing code. Remove excess allocations.

--- qualifying/the-holy-code.fsx
+++ qualifying/the-holy-code.fsx
@@ -1,5 +1,6 @@
 open System.IO
 open System
+open System.Diagnostics
 
 [<Struct>]
 type Orientation = H | V
@@ -12,20 +13,28 @@ type Image = {
     Tags: string Set
 }
 
-type SlideShow = Image list []
+type SlideShow = Image list list
 
-let solveIt (data: string list) =
+let solveIt (data: string list) () =
     let parseLine idx l =
         let O(o) :: _ :: tags = List.ofArray l
         {Id = uint32 idx; O = o; Tags = set tags}
     let data = data |> List.mapi (fun idx x -> x.Split(' ') |> parseLine idx)
 
-    let slideShows: SlideShow = data |> List.map List.singleton |> Array.ofList
+    let slideShows: SlideShow = data |> List.map List.singleton
 
     slideShows
     |> Seq.map (Seq.map (fun {Id = idx} -> string idx) >> String.concat " ") |> Seq.append [string slideShows.Length]
 
 // MAGIC ENDS HERE
+
+let timeIt label f =
+    let sw = Stopwatch()
+    sw.Start()
+    let x = f()
+    sw.Stop()
+    printfn "Time for %s: %O" label sw.Elapsed
+    x
 
 Environment.CurrentDirectory
 |> Directory.EnumerateFiles
@@ -34,5 +43,5 @@ Environment.CurrentDirectory
 |> Seq.iter (fun x -> 
     let data = File.ReadAllLines x |> List.ofArray |> List.tail
     let outputFileName = Path.ChangeExtension(x, ".out")
-    let theHolySolution = solveIt data
+    let theHolySolution = solveIt data |> timeIt x
     File.WriteAllLines(outputFileName, theHolySolution))
